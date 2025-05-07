67e1afe4c441ee8d3314817bcbcc982a7952ab24
gmltn328 <gmltn328@gmail.com>
2025-05-07 15:16:00 +0900

    scroll.js 리팩토링

--- netflix_clone/scroll.js
+++ netflix_clone/scroll.js
@@ -1,13 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const header = document.querySelector(".header-container");
+  const header = document.querySelector(".header-container"); // 헤더 컨테이너 클래스 선택
 
-  function updateHeaderTransparency() {
+  const updateHeaderTransparency = () => {
+    // 스크롤 위치가 0일 때
     if (window.scrollY === 0) {
-      header.classList.add("transparent");
+      header.classList.add("transparent"); // 헤더를 투명하게
     } else {
       header.classList.remove("transparent");
     }
-  }
+  };
 
   window.addEventListener("scroll", updateHeaderTransparency);
   updateHeaderTransparency(); // 초기 상태도 체크
