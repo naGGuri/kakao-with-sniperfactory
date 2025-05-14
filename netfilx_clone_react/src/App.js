807be89ade08b73a2ddb4aff0743666e875082ee
gmltn328 <gmltn328@gmail.com>
2025-05-14 13:32:40 +0900

    fix: 'React' must be in scope when using JSX  react/react-in-jsx-scope

--- netfilx_clone_react/src/App.js
+++ netfilx_clone_react/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
-
+import WelcomPage from "./pages/WelcomPage";
+import "./App.css";
 function App() {
   return (
-    <>
-      <MainPage />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<WelcomPage />} />
+        <Route path="/main" element={<MainPage />} />
+      </Routes>
+    </Router>
   );
 }
 
--- netfilx_clone_react/src/Pages/WelcomPage.js
+++ netfilx_clone_react/src/Pages/WelcomPage.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 import Footer from "../components/welcome/Footer";
 import Header from "../components/welcome/Header";
 import Main from "../components/welcome/Main";
